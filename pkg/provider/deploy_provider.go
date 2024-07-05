a2ff0ec20a1a85f7ebe5a6be28edc7558699c1b2
KH <>
2024-07-05 15:13:49 -0700

    Added assumerole

--- pkg/provider/deploy_provider.go
+++ pkg/provider/deploy_provider.go
@@ -7,8 +7,11 @@ import (
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/credentials/stscreds"
 	"github.com/aws/aws-sdk-go-v2/service/ec2"
 	"github.com/aws/aws-sdk-go-v2/service/resourcegroupstaggingapi"
+	taggingTypes "github.com/aws/aws-sdk-go-v2/service/resourcegroupstaggingapi/types"
+	"github.com/aws/aws-sdk-go-v2/service/sts"
 	"github.com/capillariesio/capillaries-deploy/pkg/cld/cldaws"
 	"github.com/capillariesio/capillaries-deploy/pkg/l"
 	"github.com/capillariesio/capillaries-deploy/pkg/prj"
@@ -20,8 +23,6 @@ type AwsCtx struct {
 	TaggingClient *resourcegroupstaggingapi.Client
 }
 
-const TagCapiDeploy string = "CapiDeploy"
-
 type DeployCtx struct {
 	//PrjPair   *prj.ProjectPair
 	Project   *prj.Project
@@ -32,6 +33,7 @@ type DeployCtx struct {
 }
 type DeployProvider interface {
 	GetCtx() *DeployCtx
+	ListDeployments() (l.LogMsg, error)
 	ListDeploymentResources() (l.LogMsg, error)
 	CreateFloatingIps() (l.LogMsg, error)
 	DeleteFloatingIps() (l.LogMsg, error)
@@ -62,11 +64,25 @@ func (p *AwsDeployProvider) GetCtx() *DeployCtx {
 	return p.Ctx
 }
 
-func DeployProviderFactory(project *prj.Project, goCtx context.Context, isVerbose bool) (DeployProvider, error) {
+type AssumeRoleConfig struct {
+	RoleArn    string
+	ExternalId string
+}
+
+func DeployProviderFactory(project *prj.Project, goCtx context.Context, assumeRoleCfg *AssumeRoleConfig, isVerbose bool) (DeployProvider, error) {
 	if project.DeployProviderName == prj.DeployProviderAws {
 		cfg, err := config.LoadDefaultConfig(goCtx)
 		if err != nil {
 			return nil, err
+		}
+
+		if assumeRoleCfg != nil && assumeRoleCfg.RoleArn != "" {
+			creds := stscreds.NewAssumeRoleProvider(sts.NewFromConfig(cfg), assumeRoleCfg.RoleArn,
+				func(o *stscreds.AssumeRoleOptions) {
+					o.ExternalID = aws.String(assumeRoleCfg.ExternalId)
+					o.RoleSessionName = "third-party-capideploy-assumes-role-provided-by-customer"
+				})
+			cfg.Credentials = aws.NewCredentialsCache(creds)
 		}
 
 		return &AwsDeployProvider{
@@ -74,7 +90,9 @@ func DeployProviderFactory(project *prj.Project, goCtx context.Context, isVerbos
 				Project:   project,
 				GoCtx:     goCtx,
 				IsVerbose: isVerbose,
-				Tags:      map[string]string{TagCapiDeploy: project.DeploymentName},
+				Tags: map[string]string{
+					cldaws.DeploymentNameTagName:     project.DeploymentName,
+					cldaws.DeploymentOperatorTagName: cldaws.DeploymentOperatorTagValue},
 				Aws: &AwsCtx{
 					Ec2Client:     ec2.NewFromConfig(cfg),
 					TaggingClient: resourcegroupstaggingapi.NewFromConfig(cfg),
@@ -105,13 +123,52 @@ export BASTION_IP=%s
 		prj.SshConfig.PrivateKeyPath,
 		prj.SshConfig.BastionExternalIp)
 }
-
-func (p *AwsDeployProvider) ListDeploymentResources() (l.LogMsg, error) {
+func (p *AwsDeployProvider) ListDeployments() (l.LogMsg, error) {
 	lb := l.NewLogBuilder(l.CurFuncName(), p.GetCtx().IsVerbose)
-	resources, err := cldaws.GetResourcesByTag(p.GetCtx().Aws.TaggingClient, p.GetCtx().Aws.Ec2Client, p.GetCtx().GoCtx, lb, p.GetCtx().Aws.Config.Region, TagCapiDeploy, p.Ctx.Project.DeploymentName)
+	resources, err := cldaws.GetResourcesByTag(p.GetCtx().Aws.TaggingClient, p.GetCtx().Aws.Ec2Client, p.GetCtx().GoCtx, lb, p.GetCtx().Aws.Config.Region,
+		[]taggingTypes.TagFilter{{Key: aws.String(cldaws.DeploymentOperatorTagName), Values: []string{cldaws.DeploymentOperatorTagValue}}}, false)
 	if err != nil {
 		return lb.Complete(err)
 	}
-	fmt.Printf("%s\n", strings.Join(resources, "\n"))
+	deploymentResCount := map[string]int{}
+	for _, res := range resources {
+		if deploymentNameCount, ok := deploymentResCount[res.DeploymentName]; ok {
+			deploymentResCount[res.DeploymentName] = deploymentNameCount + 1
+		} else {
+			deploymentResCount[res.DeploymentName] = 1
+		}
+	}
+	deploymentStrings := make([]string, len(deploymentResCount))
+	deploymentIdx := 0
+	totalResourceCount := 0
+	for deploymentName, deploymentResCount := range deploymentResCount {
+		deploymentStrings[deploymentIdx] = fmt.Sprintf("%s,%d", deploymentName, deploymentResCount)
+		deploymentIdx++
+		totalResourceCount += deploymentResCount
+	}
+	fmt.Printf("%s\n", strings.Join(deploymentStrings, "\n"))
+	fmt.Printf("Deployments: %d, resources: %d\n", len(deploymentResCount), totalResourceCount)
+	return lb.Complete(nil)
+}
+
+func (p *AwsDeployProvider) ListDeploymentResources() (l.LogMsg, error) {
+	lb := l.NewLogBuilder(l.CurFuncName(), p.GetCtx().IsVerbose)
+	resources, err := cldaws.GetResourcesByTag(p.GetCtx().Aws.TaggingClient, p.GetCtx().Aws.Ec2Client, p.GetCtx().GoCtx, lb, p.GetCtx().Aws.Config.Region,
+		[]taggingTypes.TagFilter{
+			{Key: aws.String(cldaws.DeploymentOperatorTagName), Values: []string{cldaws.DeploymentOperatorTagValue}},
+			{Key: aws.String(cldaws.DeploymentNameTagName), Values: []string{p.Ctx.Project.DeploymentName}}}, true)
+	if err != nil {
+		return lb.Complete(err)
+	}
+	resourceStrings := make([]string, len(resources))
+	activeCount := 0
+	for resIdx, res := range resources {
+		resourceStrings[resIdx] = res.String()
+		if res.BilledState != cldaws.ResourceBilledStateTerminated {
+			activeCount++
+		}
+	}
+	fmt.Printf("%s\n", strings.Join(resourceStrings, "\n"))
+	fmt.Printf("Total: %d, potentially billed: %d\n", len(resources), activeCount)
 	return lb.Complete(nil)
 }
